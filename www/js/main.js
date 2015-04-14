a7002c92b04040c8d2000739e94efa0c64d6a4e0
Omar Himada <contact@omarhimada.com>
2015-04-14 12:41:01 -0400

    Modifications to CSS and JS
    Mostly coding standards compliance changes

---
+++ www/js/main.js
@@ -0,0 +1,104 @@
+document.addEventListener("deviceready", onDeviceReady, false);
+function onDeviceReady() {
+    var list,
+    kills,
+    count = 0,
+    email = "",
+    password = "";
+    bluetoothle.initialize(initializeSuccess, initializeError, {
+        "request": true,
+        "statusReceiver": false
+    });
+    function initializeSuccess() {
+
+    }
+    function initializeError() {
+
+    }
+    function stopScanSuccess() {
+        //var jsonString = JSON.stringify(list);
+        email = window.localStorage.getItem("BThereEmail");
+        if (list == "CoffeeLoft") {
+            $.ajax({
+                type: "POST",
+                async: "true",
+                url: "http://www.greenseedmusic.com/btherecheckin.php",
+                data: {
+                    email: email,
+                    locArr: list
+                },
+                success: function (data) {
+                    alert(JSON.parse(data));
+                },
+                error: function (data) {
+                    alert("Error");
+                }
+            });
+        } else {
+            alert("You may only use this application from the Coffee Loft.");
+            document.getElementById("checkin").disabled = false;
+        }
+    }
+    function stopScanError() {
+
+    }
+    function startScanSuccess(obj) {
+        document.getElementById("checkin").disabled = true;
+        myVar = setTimeout(function () {
+            clearTimeout(kills);
+            bluetoothle.stopScan(stopScanSuccess, stopScanError)
+        }, 6000);
+        if (obj.name == "CoffeeLoft" && count < 1) {
+            list = obj.name;
+            count++;
+        }
+    }
+    function startScanError() {
+
+    }
+
+    document.getElementById("join").addEventListener("click", function() {
+        (document.getElementById("buttons")).style.display = "none";
+        (document.getElementById("signupformsection")).style.display = "none";
+    });
+    document.getElementById("checkin").addEventListener("click", function() {
+        alert("Check-in success!");
+        kills = setTimeout(function () {
+            bluetoothle.startScan(startScanSuccess, startScanError, {
+                "serviceUuids": []
+            });
+        }, 500);
+    });
+
+    if (window.localStorage.getItem("BThere") == "true") {
+        email = window.localStorage.getItem("BThereEmail");
+        password = window.localStorage.getItem("BTherePassword");
+        $.ajax({
+            type: "POST",
+            async: "true",
+            url: "http://www.greenseedmusic.com/btherelogin.php",
+            data: {
+                email: email,
+                password: password
+            },
+            success: function (data) {
+                (document.getElementById("checkinsection")).style.display = "block";
+            },
+            error: function (data) {}
+        });
+    } else {
+        (document.getElementById("buttons")).style.display = "block";
+    }
+    $('#Signup').submit(function () {
+        $.post('http://www.greenseedmusic.com/bthereinsert.php', $(this).serialize(), function (data) {
+            email = $("#signupEmail").val();
+            password = $("#signupPassword").val();
+            window.localStorage.setItem("BThere", "true");
+            window.localStorage.setItem("BThereEmail", email);
+            window.localStorage.setItem("BTherePassword", password);
+        }).fail(function () {
+
+        });
+        return false;
+    });
+}
